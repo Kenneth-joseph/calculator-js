515255dfb9da27560fc871a10129cfb89c34f4d0
Kenneth-Joseph <josephochieng812@gmail.com>
2021-04-01 21:09:33 +0300

    finishing up on compute function

--- style.js
+++ style.js
@@ -1,14 +1,15 @@
+// const { delete } = require("request")
 
 
 const numberButtons= document.querySelectorAll('.number')
 const triggerButtons= document.querySelectorAll('.trigger')
 const deleteButton=document.querySelector("#delete")
 const clearButton = document.querySelector("#clear")
-const equalButton = document.querySelector("#equa")
+const equalButton = document.querySelector("#equal")
 const previousInputTextElement= document.querySelector('#previous-input')
 const currentInputTextElement= document.querySelector('#current-input')
 
-console.log(clearButton)
+// console.log(clearButton)
 // console.log(equalButton)
 // Creating a class constructor for building other calculators
 class Calculator{
@@ -30,7 +31,7 @@ class Calculator{
 
 // this is gonna help with deleting characters
 delete(){
- 
+ console.log("ALMOST THERE")
 }
 
 // this will append values in the result, currentinput section
@@ -53,7 +54,28 @@ chooseTrigger(operation){
 // to solve or find compution values will call for this fucntion
 
 compute(){
+   let result
+   const current= parseFloat(this.currentInput)
+   const previous = parseFloat(this.previousInput)
+   if (isNaN(current) || isNaN(previous)) return
 
+   switch(this.operation){
+       case '+':
+           result= previous + current
+           break
+        
+       case '*':
+           result= previous * current
+           break
+
+       case '-':
+          result= previous + current
+          break
+
+       case '+':
+           result= previous + current
+           break
+   }
 }
 
 //    update display wil help show the results after being computed
@@ -86,7 +108,14 @@ triggerButtons.forEach(button=>{
     })
 
 
-// deleteButton.addEventListener('click'=()=>{
-//     console.log("hello man")
-// })
+deleteButton.addEventListener('click', button =>{
+    calculator.delete()
+    // console.log("ALMOST THERE")
+})
+
+equalButton.addEventListener('click', button =>{
+    calculator.compute()
+    calculator.updateDisplay()
+    console.log("sum")
+})
 // alert("hey i am okay")
