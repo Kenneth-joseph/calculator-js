1f90e75b8cf7b9db78ed459b0672bc90f64ce369
Kenneth-Joseph <josephochieng812@gmail.com>
2021-04-01 20:04:23 +0300

    working on operations choices

--- style.js
+++ style.js
@@ -4,10 +4,12 @@ const numberButtons= document.querySelectorAll('.number')
 const triggerButtons= document.querySelectorAll('.trigger')
 const deleteButton=document.querySelector("#delete")
 const clearButton = document.querySelector("#clear")
+const equalButton = document.querySelector("#equa")
 const previousInputTextElement= document.querySelector('#previous-input')
 const currentInputTextElement= document.querySelector('#current-input')
 
-
+console.log(clearButton)
+// console.log(equalButton)
 // Creating a class constructor for building other calculators
 class Calculator{
    constructor(previousInputTextElement,currentInputTextElement){
@@ -25,10 +27,12 @@ class Calculator{
     this.operation= undefined
 
 }
+
 // this is gonna help with deleting characters
 delete(){
-
+ 
 }
+
 // this will append values in the result, currentinput section
 appendNUmber(number){
  if(number=='.' && this.currentInput.includes('.')) return
@@ -37,7 +41,13 @@ appendNUmber(number){
 
 // this is gonna help withn the selected operation
 chooseTrigger(operation){
-
+ if(this.currentInput==='') return
+ if(this.previousInput !== ''){
+     this.compute()
+ }
+ this.operation = operation
+ this.previousInput = this.currentInput
+ this.currentInput=''
 }
 
 // to solve or find compution values will call for this fucntion
@@ -49,6 +59,8 @@ compute(){
 //    update display wil help show the results after being computed
 updateDisplay(){
  this.currentInputTextElement.innerText= this.currentInput
+ this.previousInputTextElement.innerText= this.previousInput
+ 
 }
 
 }
@@ -63,4 +75,18 @@ button.addEventListener( 'click',()=>{
  
 })
 })
-alert("hey i am okay")
+
+triggerButtons.forEach(button=>{
+    button.addEventListener( 'click',()=>{
+        // console.log(button.innerText)
+        calculator.chooseTrigger(button.innerText)
+        calculator.updateDisplay()
+       
+    })
+    })
+
+
+// deleteButton.addEventListener('click'=()=>{
+//     console.log("hello man")
+// })
+// alert("hey i am okay")
